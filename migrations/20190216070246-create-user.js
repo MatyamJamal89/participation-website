8fd0f1f9ed6d5004cbb4fa7350171bfe87214d2c
Ehsan Zumrut <ehsan@tamhub.com>
2019-02-26 11:50:11 +0300

    Add a migration file to create a table for users

---
+++ migrations/20190216070246-create-user.js
@@ -0,0 +1,47 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      name: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      last_login: {
+        type: Sequelize.DATE
+      },
+      type: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      role: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
