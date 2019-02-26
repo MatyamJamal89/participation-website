90e05c846ca491a91d574d8e6429c264f0186f2f
Ehsan Zumrut <ehsan@tamhub.com>
2019-02-26 11:51:34 +0300

    Use الصفحة الرئيسية instead of Wameedh Association as a title

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Wameedh Association' });
+router.get('/', function(req, res,) {
+  res.render('index', { title: 'الصّفحة الرئيسية' });
 });
 
 module.exports = router;
